ec0d0e71dec5875fbbd9700ee0ceaf6685c006f3
tmokmss <tomookam@live.jp>
2018-03-26 00:46:17 +0900

    add light power on/off feature

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ghome_server
+A project to make my house smart again.
+
+## Install
+```
+$ clone https://github.com/tmokmss/ghome_server.git
+$ export GHOME_ADDR="[your google home ip address]"
+```
+
+## How to use
+```
+$ cd ghome_server
+$ npm install
+$ pm2 start main.js 
+```
+
+Also you need to configure IFTTT to post requests to this server with some trigger. 
--- main.js
+++ main.js
@@ -1,14 +1,42 @@
+const exec = require('child_process').exec;
+
+const googlehome = require('google-home-notifier')
+const language = 'ja';
+const addr = process.env.GHOME_ADDR;
+googlehome.ip(addr, language);
+
 express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.json({ name: 'テストユーザー' });
+  googlehome.notify('了解です', function (res2) {
+    console.log(res2);
+  });
+  //googlehome.play('https://tmokmss.ddns.net/captures/wolf.mp3', function (res2) {
+  //  console.log(res2);
+  //});
 });
 
-app.get('/test', function(req, res){
+app.get('/lightoff', function (req, res) {
   res.set('Content-Type', 'application/json');
   res.json({ name: 'test user' });
+  exec('irsend SEND_ONCE light LightOff', (err, stdout, stderr) => {
+  });
+  googlehome.notify("電気を消しました", function (res2) {
+    console.log(res2);
+  });
+});
+
+app.get('/lightOn', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.json({ name: 'test user' });
+  exec('irsend SEND_ONCE light LightFavorite', (err, stdout, stderr) => {
+  });
+  googlehome.notify("電気を付けました", function (res2) {
+    console.log(res2);
+  });
 });
 
 app.listen(9000);
